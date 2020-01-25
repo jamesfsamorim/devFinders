7ad0c40a1775ca2a8e5d76a78fddfaeda1b16222
James Amorim <jamesfsamorim@gmail.com>
2020-01-24 22:47:49 -0300

    Add socket io to when register in web will appear in mobile the new dev, if matched techs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+setupWebsocket(server)
+
 mongoose.connect('mongodb+srv://omni:omniadmin@tindevcluster-mt9dj.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -12,4 +17,4 @@ mongoose.connect('mongodb+srv://omni:omniadmin@tindevcluster-mt9dj.mongodb.net/t
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.listen(3333)
+server.listen(3333)
